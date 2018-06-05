9a385e9ef0e28510049d1c0bf274772d18ffe8d8
peln <pern9378@stud.kea.dk>
2018-06-05 13:26:37 +0200

    edit working

--- main.js
+++ main.js
@@ -42,12 +42,47 @@ database.ref("notes/").on("child_added", (snapshot)=>{
         database.ref("notes/"+key).remove();
     });
 
+    clone.querySelector("button.edit").addEventListener("click", e=>{
+        const tempTemplate = document.querySelector("#editFormTemplate").content;
+        const formClone = tempTemplate.cloneNode(true);
+        const theArticle = e.target.parentElement.parentElement;
+        const theDiv = theArticle.querySelector("div").textContent;
+        const theHeader = theArticle.querySelector("h1").textContent;
+        const theKey = theArticle.dataset.key;
+        console.log(theDiv, theHeader, theKey);
+
+        formClone.querySelector("input").value = theHeader;
+        formClone.querySelector("textarea").value = theDiv;
+        formClone.querySelector("form").addEventListener("submit", e=>{
+            e.preventDefault();
+
+            let lastForm = document.querySelector("footer+form");
+            console.log(lastForm);
+
+            database.ref("notes/"+theKey).set({
+                header: lastForm.querySelector("input").value,
+                description: lastForm.querySelector("textarea").value
+            });
+
+        
+            theArticle.querySelector("h1").textContent=lastForm.querySelector("input").value;
+            theArticle.querySelector("div").textContent=lastForm.querySelector("textarea").value;
+
+            lastForm.remove();
+
+        });
+        
+        theArticle.appendChild(formClone);
+        app.appendChild(formClone);
+
+    });
 
     app.appendChild(clone);
 
 });
 
 
+//update UI when stuff is deleted
 //listen for removal of data child_removed
 database.ref("notes/").on("child_removed", snapshot=>{
     const key = snapshot.key;
@@ -55,45 +90,22 @@ database.ref("notes/").on("child_removed", snapshot=>{
     let el = document.querySelector(`article[data-key=${key}]`);
 
     el.remove();
-    console.log(el);
-
-
-})
-
+    // console.log(el);
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-//update UI when stuff is deleted
-
-
-//underneath each post - edit and delete
-
-
-//childremoved event
+// 1 template form
+// 2 append where/when needed
+// 3 populate form
+// 4 do stuff on some
 
 
 
 
 
 
-
-
-
-
-
-
-
-// you should not be allowed to submit an empty form
 
 
 
